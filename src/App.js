5c0fb6b753c6f6426d3f0126ff5c74358fcf5fe2
Hasul79 <hatuk2@mail.ru>
2023-01-13 22:29:12 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { FaStar } from 'react-icons/fa';
+
+
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9"
+}
 
 function App() {
+  const stars = Array(5).fill(0);
+
+const [currentValue, setCurrentValue] = useState(0);
+const [hoverValue, setHoverValue] = useState(undefined);
+
+const handleClick = value => {
+  setCurrentValue(value)
+};
+
+const handleMouseOver = value => {
+  setHoverValue(value)
+}
+
+const handleMouseLeave = () => {
+  setHoverValue(undefined)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+     <h2>Star Ratings in React</h2>
+     <div style={styles.stars}>
+      {stars.map((_, index) => {
+        return (
+        <FaStar key={index} 
+        size={24}
+        style={{marginRight: 10, cursor: 'pointer'}}
+
+        color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+        onClick={() => handleClick(index + 1)}
+        onMouseOver={() => handleMouseOver(index + 1)}
+        onMouseLeave={handleMouseLeave}
+        />
+        )
+      })}
+     
+     </div>
+     <textarea placeholder="What's your feedback" 
+       style={styles.textarea} />
+     <button style={styles.button}>Submit</button>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  }, 
+  textarea: {
+     border: "1px solid #a9a9a9",
+     borderRadius: 5,
+     width: 300,
+     margin: "20px 0",
+     minHeight: 100,
+     padding: 10
+  },
+  button: {
+    border: "1px solid #a9a9a9",
+     borderRadius: 5,
+     width: 300,
+     padding: 10,
+     cursor: "pointer"
+  }
 }
 
 export default App;
